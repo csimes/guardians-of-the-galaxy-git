e77992173d76e375010f2169d5b765c8914bccd6
Jared Upton <jalupto@gmail.com>
2021-07-05 20:16:47 -0400

    finished weather and ticket api

--- guardians-group-project/src/App.js
+++ guardians-group-project/src/App.js
@@ -2,23 +2,28 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Location from "./components/site/Location";
-// import NasaApp from "./components/apps/nasa/NasaApp";
+import NasaApp from "./components/apps/nasa/NasaApp";
 import OpenWeatherApp from "./components/apps/open-weather/OpenWeatherApp";
 import AppToggle from "./components/site/Header";
 import Footer from "./components/site/Footer";
-// import TicketMasterApp from "./components/apps/ticket-master/TicketMasterApp";
+import TicketMasterApp from "./components/apps/ticket-master/TicketMasterApp";
 
 const App = () => {
 
     return (
         <div className="App">
             <br/>
-            <AppToggle />
+            <h2>Plan your next event!</h2>
             <br/>
             <Location />
+            <br/>
+            <AppToggle />
+            <br/>
             <OpenWeatherApp/>
-            {/* <NasaApp /> */}
-            {/* <TicketMasterApp /> */}
+            <br/>
+            <NasaApp />
+            <br/>
+            <TicketMasterApp />
             <br/>
             <Footer />
         </div>
--- guardians-group-project/src/components/apps/open-weather/OpenWeatherApp.js
+++ guardians-group-project/src/components/apps/open-weather/OpenWeatherApp.js
@@ -24,7 +24,6 @@ function OpenWeatherApp() {
         setTemp(temp);
         setFeels(feels_like);
         setOverall(overall);
-        console.log(data);
     };
     
     useEffect(() => {
